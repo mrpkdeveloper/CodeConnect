8879dbc96f14a3bd70bd79f5d8b3a03e7bc76824
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-10-03 23:41:34 +0530

    turn server

--- public/script.js
+++ public/script.js
@@ -1,29 +1,30 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
-// const myPeer = new Peer(undefined, {
-//   path: "/peerjs",
-//   host: "/",
-//   port: "3030", //443 for production
-// });
-
-// if(!ice){
-//   ice = new $xirsys.ice('/webrtc');
-//   ice.on(ice.onICEList, function (evt){
-//       console.log('onICE ',evt);
-//       if(evt.type == ice.onICEList){
-//           create(ice.iceServers);
-//       }
-//   });
-// }
-
-const myPeer = new Peer(
-  { key: "lwjd5qra8257b9", debug: 3, config: ice.iceServers }
-  // {
-  //   path: "/peerjs",
-  //   host: "/",
-  //   port: "443", //443 for production
-  // }
-);
+const myPeer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "443", //443 for production
+  config: {
+    iceServers: [
+      {
+        urls: ["stun:bn-turn1.xirsys.com"],
+      },
+      {
+        username:
+          "CWAMDU_IwwRmty8iMrAactysEXO6eP4M85T3Xq01pSnH8FvidtyXGHp0nEg2L32WAAAAAF94vXxtcnBrZGV2ZWxvcGVy",
+        credential: "109ad15c-05a3-11eb-8f6a-0242ac140004",
+        urls: [
+          "turn:bn-turn1.xirsys.com:80?transport=udp",
+          "turn:bn-turn1.xirsys.com:3478?transport=udp",
+          "turn:bn-turn1.xirsys.com:80?transport=tcp",
+          "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+          "turns:bn-turn1.xirsys.com:443?transport=tcp",
+          "turns:bn-turn1.xirsys.com:5349?transport=tcp",
+        ],
+      },
+    ],
+  },
+});
 
 const myroomid = ROOM_ID;
 let myVideoStream;
