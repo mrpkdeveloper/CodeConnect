40de70d24a97e9e6b210a860b58c397dedfb899e
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2021-11-17 20:50:39 +0530

    added turn server and setting things up for production

--- public/script.js
+++ public/script.js
@@ -4,6 +4,24 @@ const myPeer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
   port: "443",
+  config: {
+    iceServers: [
+      { urls: ["stun:bn-turn1.xirsys.com"] },
+      {
+        username:
+          "OqZByxJ5ZrKFXYdws9gizGoLdQyhYg5kQh4mMTFKi2TVFY-5kFogetZAu6Ho1369AAAAAGGT_OltcnBrZGV2ZWxvcGVy",
+        credential: "be98eaf8-470d-11ec-ac21-0242ac140004",
+        urls: [
+          "turn:bn-turn1.xirsys.com:80?transport=udp",
+          "turn:bn-turn1.xirsys.com:3478?transport=udp",
+          "turn:bn-turn1.xirsys.com:80?transport=tcp",
+          "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+          "turns:bn-turn1.xirsys.com:443?transport=tcp",
+          "turns:bn-turn1.xirsys.com:5349?transport=tcp",
+        ],
+      },
+    ],
+  },
 });
 const myroomid = ROOM_ID;
 const myname = Name;
