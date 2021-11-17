7c3eab8d9fef729059ec9a7a0d05e4677ed6caf6
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2021-11-17 09:50:01 +0530

    error

--- public/script.js
+++ public/script.js
@@ -4,24 +4,6 @@ const myPeer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
   port: "443", //443 for production
-  config: {
-    iceServers: [
-      { urls: ["stun:bn-turn1.xirsys.com"] },
-      {
-        username:
-          "OqZByxJ5ZrKFXYdws9gizGoLdQyhYg5kQh4mMTFKi2TVFY-5kFogetZAu6Ho1369AAAAAGGT_OltcnBrZGV2ZWxvcGVy",
-        credential: "be98eaf8-470d-11ec-ac21-0242ac140004",
-        urls: [
-          "turn:bn-turn1.xirsys.com:80?transport=udp",
-          "turn:bn-turn1.xirsys.com:3478?transport=udp",
-          "turn:bn-turn1.xirsys.com:80?transport=tcp",
-          "turn:bn-turn1.xirsys.com:3478?transport=tcp",
-          "turns:bn-turn1.xirsys.com:443?transport=tcp",
-          "turns:bn-turn1.xirsys.com:5349?transport=tcp",
-        ],
-      },
-    ],
-  },
 });
 // console.log(CONFIG);
 const myroomid = ROOM_ID;
