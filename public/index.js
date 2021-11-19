6fa20f564911805150c852392ead6b1752f87171
anirudhbelwadi <anirudh.belwadi@gmail.com>
2021-11-20 00:33:10 +0400

    home page made responsive

--- public/index.js
+++ public/index.js
@@ -3,18 +3,22 @@ let room_btn = document.getElementById("joinroom");
 new_btn.addEventListener("click", function () {
   // console.log("hello");
   document.querySelector(".bg-modal-menu").style.display = "flex";
+  document.querySelector("body").style.overflow = "hidden";
 });
 
 room_btn.addEventListener("click", function () {
   console.log("hello");
   document.querySelector(".bg-modal-menu-2").style.display = "flex";
+  document.querySelector("body").style.overflow = "hidden";
 });
 
 document.querySelector(".close-menu").addEventListener("click", function () {
   console.log("close clicked");
   document.querySelector(".bg-modal-menu").style.display = "none";
+  document.querySelector("body").style.overflow = "visible";
 });
 document.querySelector(".close-menu-2").addEventListener("click", function () {
   console.log("close clicked");
   document.querySelector(".bg-modal-menu-2").style.display = "none";
+  document.querySelector("body").style.overflow = "visible";
 });
